92306562adae4932c233e904f5f7422b02ab905e
Darryl L. Miles <darryl.miles@darrylmiles.org>
2023-10-28 19:31:41 +0100

    build: sky130_fd_sc_hd__dlrtp.v sky130_behavioral_udp_dlatch_pr.v

---
+++ src/sky130_toolbox/sky130_behavioral_udp_dlatch_pr.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+// udp_dlatch$PR
+module sky130_fd_sc_hd__udp_dlatch$PR (
+    Q    ,
+    D    ,
+    GATE ,
+    RESET
+);
+
+    output Q    ;
+    input  D    ;
+    input  GATE ;
+    input  RESET;
+
+    reg Q;
+
+    always_latch begin
+        if (RESET) begin
+            Q <= 0;		// immediate async
+        end else if (GATE) begin
+`ifdef TIMING
+            // TIMING is not allowed inside always_latch
+            //#1			// at next simtime tick
+`endif
+            Q <= D;
+        end
+    end
+
+endmodule
